baeabb50ddf9b013a816361b28711cc0b6aa3217
TheFixDev <86165866+TheFixDev@users.noreply.github.com>
2021-09-10 19:30:06 +0200

    Added Plugin

---
+++ Version/src/TheFixDev/Version/Main.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace TheFixDev\Version\Main;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
+
+class main extends PluginBase{
+    public function onEnable()
+    {
+        Server::getInstance()->getCommandMap()->unregister(Server::getInstance()->getCommandMap()->getCommand("version"));
+        $this->getLogger()->notice("The Command /version and /about have been deactivated");
+    }
+}
