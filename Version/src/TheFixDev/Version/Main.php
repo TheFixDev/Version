9cde69c22438bed66653694d9682c30dded8ede9
TheFixDev <86165866+TheFixDev@users.noreply.github.com>
2021-09-11 18:54:09 +0200

    Update Main.php

--- Version/src/TheFixDev/Version/Main.php
+++ Version/src/TheFixDev/Version/Main.php
@@ -9,6 +9,8 @@ class main extends PluginBase{
     public function onEnable()
     {
         Server::getInstance()->getCommandMap()->unregister(Server::getInstance()->getCommandMap()->getCommand("version"));
-        $this->getLogger()->notice("The Command /version and /about have been deactivated");
+        $this->getLogger()->notice("The Command /version and /about have been deleted");
     }
+    
+    
 }
