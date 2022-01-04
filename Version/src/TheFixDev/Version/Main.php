c97e987ce85b0c867f2014511da513ae88c509bf
GamerMJay <66413999+GamerMJay2008@users.noreply.github.com>
2022-01-04 02:32:02 +0100

    Update Main.php
    Update code

--- Version/src/TheFixDev/Version/Main.php
+++ Version/src/TheFixDev/Version/Main.php
@@ -9,8 +9,5 @@ class main extends PluginBase{
     public function onEnable(): void
     {
         Server::getInstance()->getCommandMap()->unregister(Server::getInstance()->getCommandMap()->getCommand("version"));
-        $this->getLogger()->notice("Command /version successfully deleted");
     }
-
-
 }
